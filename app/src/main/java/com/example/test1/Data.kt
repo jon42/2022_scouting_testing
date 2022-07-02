027d9f97cbd9cfbca75f510144acb08357847da5
Big Thick <jonw3821@outlook.com>
2022-07-02 15:28:47 -0700

    Working climb visuals

--- app/src/main/java/com/example/test1/Data.kt
+++ app/src/main/java/com/example/test1/Data.kt
@@ -3,17 +3,17 @@ package com.example.test1
 import kotlinx.serialization.Serializable
 
 @Serializable
-class Data (Team: Int, Pose: String, Match: Int) {
+data class Data (val Team: Int, val Pose: String,val Match: Int) {
 
+        private var autoHighGoal = 0
+        private var autoLowGoal = 0
+        private var teleHighGoal = 0
+        private var teleLowGoal = 0
+        private var climb = ""
+        private var foul = 0
+        private var notes = ""
+        private var lastAction = mutableListOf<String>()
 
-    private var autoHighGoal = 0
-    private var autoLowGoal = 0
-    private var teleHighGoal = 0
-    private var teleLowGoal = 0
-    private var climb = 0
-    private var foul = 0
-    private var notes = ""
-    private var lastAction = mutableListOf<String>()
     fun addHighAuto(){
         autoHighGoal++
 
@@ -30,23 +30,17 @@ class Data (Team: Int, Pose: String, Match: Int) {
     fun addFoul(){
         foul++
     }
-    fun lowC(){
-        climb = 1
-    }
-    fun midC(){
-        climb = 2
-    }
-    fun highC() {
-        climb = 3
-    }
-    fun travC(){
-        climb = 4
+    fun setClimb(str: String){
+        climb = str
     }
     fun last(Str: String){
         lastAction.add(Str)
         if(lastAction.size > 15){
             lastAction.removeAt(0)
         }
+    }
+    fun getClimb(): String{
+        return climb
     }
     fun undo(){
         if(lastAction.isEmpty()) return
@@ -56,7 +50,7 @@ class Data (Team: Int, Pose: String, Match: Int) {
             "tele High" -> teleHighGoal--
             "tele Low" -> teleLowGoal--
             "foul" -> foul--
-            "climb" -> climb--
+            "climb" -> climb = ""
             "Note" -> notes =""
             }
         }
--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.test1
 
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.data_input.*
+import kotlinx.android.synthetic.main.initialize.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 //        mAuth = FirebaseAuth.getInstance()
     }
-    private val teamNumber: EditText = findViewById(R.id.AddTeamNumber)
-    private val matchNumber: EditText = findViewById(R.id.addMatchNumber)
+    lateinit var data: Data
     public override fun onStart(){
         super.onStart()
 //        var currentUser = mAuth.currentUser
@@ -26,9 +27,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.initialize)
     }
     fun bData(view: View){
-//        if(teamNumber.text == null || matchNumber.text == null) return
+        var button = findViewById<Button>(view.id)
+        if(addTeamNumber.text.toString() == "" || addMatchNumber.text.toString() == "" ) return
         setContentView(R.layout.data_input)
-//        var data: Data = Data(teamNumber.getText().toString().toInt(),"red", matchNumber.getText().toString().toInt())
+        data = Data(addTeamNumber.getText().toString().toInt(),button.text.toString(), addMatchNumber.getText().toString().toInt())
+        print(data.Match.toString() + " " + data.Team + " " + data.Pose)
+    }
+    fun addC(view: View){
+        var level = findViewById<Button>(view.id)
+        data.setClimb(level.text.toString())
+        climbTitle.text = "Climb: " + data.getClimb()
     }
     fun setToData(){
         setContentView(R.layout.data_input)
